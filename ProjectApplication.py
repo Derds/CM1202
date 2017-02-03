a2f00102064dc86be13dd0db541e435a644f8f21
Bkork <RapleyI1@cardiff.ac.uk>
2017-02-03 14:46:10 +0000

    Add files via upload
    A file with some dictionaries showing how they might work within the project. LmaoBkork

---
+++ ProjectApplication.py
@@ -0,0 +1,14 @@
+courseCodes = {
+"UFBSCMSA":"BSc Computer Science", "UFBSCMSB":"BSc Computer Science with year in industry", 
+"UFBSCSFA":"BSc Computer Science and Forensics", "UFBSCSFB":"BSc Computer Science and Forensics with year in industry",
+"UFBSCSHA":"BSc Computer Science with HPC", "UFBSCSHB":"BSc Computer Science with HPC with year in industry",
+"UFBSCVCA":"BSc Computer Science with Computer Vision and Computer Graphics",
+"UFBSCVCB":"BSc Computer Science with Computer Vision and Computer Graphics with year in industry", "UFBSASEA":"BSc Applied Software Engineering"}
+
+
+
+BloggsJ43 = {"studentCode":"0236587/1","surname":"Bloggs","forename1":"Jo",
+"forename2":"","tutor":"","course":"UFBSCMSA","AcadYear":"2001/2",
+"univEmail":"BloggsJ43@cardiff.ac.uk"}
+
+
